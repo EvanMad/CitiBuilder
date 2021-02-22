006b75bff04d5169ff5770b3871d788b1a917ca1
EvanM <evan.madurai@googlemail.com>
2021-02-22 22:48:21 +0000

    Add files via upload

---
+++ content/tiles/gen.bat
@@ -0,0 +1,33 @@
+tilegen --tex=res/station.png --size=8 --height=1 --cost=50 --tiletype=0 --anim=0,0,1 --out=void
+
+tilegen --tex=res/residential_towers.png --size=8 --height=2 --cost=50 --tiletype=1 --anim=0,0,1 --out=residential_towers
+tilegen --tex=res/residential_skyscrapers_huge.png --size=8 --height=4 --cost=50 --tiletype=2 --anim=0,0,1 --out=residential_skyscrapers_huge
+tilegen --tex=res/residential_skyscrapers.png --size=8 --height=2 --cost=50 --tiletype=3 --anim=0,0,1 --out=residential_skyscrapers
+tilegen --tex=res/residential_medium_density.png --size=8 --height=2 --cost=50 --tiletype=4 --anim=0,0,1 --out=residential_medium_density
+tilegen --tex=res/residential_low_density.png --size=8 --height=2 --cost=50 --tiletype=5 --anim=0,0,1 --out=residential_low_density
+tilegen --tex=res/residential_high_density.png --size=8 --height=2 --cost=50 --tiletype=6 --anim=0,0,1 --out=residential_high_density
+
+tilegen --tex=res/industrial_high_density.png --size=8 --height=2 --cost=50 --tiletype=7 --anim=0,0,1 --out=industrial_high_density
+tilegen --tex=res/industrial_medium_density.png --size=8 --height=2 --cost=50 --tiletype=8 --anim=0,0,1 --out=industrial_medium_density
+tilegen --tex=res/industrial_low_density.png --size=8 --height=2 --cost=50 --tiletype=9 --anim=0,0,1 --out=industrial_low_density
+
+tilegen --tex=res/commercial_high_density.png --size=8 --height=2 --cost=50 --tiletype=10 --anim=0,0,1 --out=commercial_high_density
+tilegen --tex=res/commercial_medium_density.png --size=8 --height=2 --cost=50 --tiletype=11 --anim=0,0,1 --out=commercial_medium_density
+tilegen --tex=res/commercial_low_density.png --size=8 --height=2 --cost=50 --tiletype=12 --anim=0,0,1 --out=commercial_low_density
+
+
+tilegen --tex=res/dirt.png --size=8 --height=1 --cost=50 --tiletype=13 --anim=0,0,1 --out=dirt
+tilegen --tex=res/sand.png --size=8 --height=1 --cost=50 --tiletype=14 --anim=0,0,1 --out=sand
+
+tilegen --tex=res/forest.png --size=8 --height=1 --cost=50 --tiletype=15 --anim=0,0,1 --out=forest
+
+tilegen --tex=res/grass.png --size=8 --height=1 --cost=50 --tiletype=16 --anim=0,0,1 --out=grass
+
+tilegen --tex=res/rail.png --size=8 --height=1 --cost=50 --tiletype=17 --anim=0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1 --out=rail
+tilegen --tex=res/road.png --size=8 --height=1 --cost=50 --tiletype=18 --anim=0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1 --out=road
+
+tilegen --tex=res/wheat.png --size=8 --height=1 --cost=50 --tiletype=19 --anim=0,0,1 --out=wheat
+tilegen --tex=res/tree.png --size=8 --height=2 --cost=50 --tiletype=20 --anim=0,0,1 --out=tree
+tilegen --tex=res/water.png --size=8 --height=1 --cost=50 --tiletype=21 --anim=0,3,0.5,0,3,0.5,0,3,0.5,0,3,0.5 --out=water
+
+tilegen --tex=res/space.png --size=8 --height=2 --cost=50 --tiletype=43 --anim=0,3,0.5,0,3,0.5,0,3,0.5,0,3,0.5 --out=space
